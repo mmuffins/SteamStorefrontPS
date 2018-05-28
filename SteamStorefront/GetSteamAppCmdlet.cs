9bb95836483b8cd8f0c4cfee7d2ffed0572603c9
mmuffins <mail@tiv73.com>
2018-05-28 21:56:01 +0200

    Added help text for all cmdlets.

--- SteamStorefront/GetSteamAppCmdlet.cs
+++ SteamStorefront/GetSteamAppCmdlet.cs
@@ -9,19 +9,50 @@ using System.Threading.Tasks;
 
 namespace SteamStorefront
 {
+    /// <summary>
+    /// <para type="synopsis">Retrieves information about an application in the steam store.</para>
+    /// <para type="description">The Get-SteamApp cmdlet gets information about an application in the</para>
+    /// <para type="description">steam store. The returned object contains all information about the</para>
+    /// <para type="description">application that's available on the steam storefront api.</para>
+    /// <para type="example"></para>
+    /// </summary>    
+    /// <example>
+    ///   <para>Example 1: Get an application by id</para>
+    ///   <para></para>
+    ///   <code>PS C:\>Get-SteamApp -AppId 323170</code>
+    ///   <para></para>
+    ///   <para>This command gets steamapp with id 323170.</para>
+    ///   <para></para>
+    ///   <para>Example 2: Get an application by country and language</para>
+    ///   <para></para>
+    ///   <code>PS C:\>Get-SteamApp -AppId 323170 -Region "JP" -Language "german"</code>
+    ///   <para></para>
+    ///   <para>This command gets steamapp with id 323170 with regional parameters like date format,</para>
+    ///   <para>price or currency for the japanese steam store, with strings localized to german.</para>
+    ///   <para></para>
+    /// </example>
     [Cmdlet(VerbsCommon.Get, "SteamApp")]
     [OutputType(typeof(SteamApp))]
     public class GetSteamAppCmdlet : Cmdlet
     {
         #region properties
+        /// <summary>
+        /// <para type="description">Steam Package ID</para>
+        /// </summary>
         [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
         [Alias("id", "packageid")]
         public int AppId { get; set; }
 
+        /// <summary>
+        /// <para type="description">Two letter country code to customise currency and date values.</para>
+        /// </summary>
         [Parameter(Position = 1, ValueFromPipelineByPropertyName = true)]
-        [Alias("country")]
+        [Alias("country", "cc")]
         public string Region { get; set; }
 
+        /// <summary>
+        /// <para type="description">Full name of the language in english used for string localization e.g. name, description.</para>
+        /// </summary>
         [Parameter(Position = 2, ValueFromPipelineByPropertyName = true)]
         public string Language { get; set; }
 
--- SteamStorefront/GetSteamPackageCmdlet.cs
+++ SteamStorefront/GetSteamPackageCmdlet.cs
@@ -9,19 +9,51 @@ using System.Threading.Tasks;
 
 namespace SteamStorefront
 {
+
+    /// <summary>
+    /// <para type="synopsis">Retrieves information about a package in the steam store.</para>
+    /// <para type="description">The Get-SteamPackage cmdlet gets information about a package in the</para>
+    /// <para type="description">steam store. The returned object contains all information about the</para>
+    /// <para type="description">package that's available on the steam storefront api.</para>
+    /// <para type="example"></para>
+    /// </summary>    
+    /// <example>
+    ///   <para>Example 1: Get an application by id</para>
+    ///   <para></para>
+    ///   <code>PS C:\>Get-SteamPackage -PackageId 73194</code>
+    ///   <para></para>
+    ///   <para>This command gets package with id 73194.</para>
+    ///   <para></para>
+    ///   <para>Example 2: Get an application by country and language</para>
+    ///   <para></para>
+    ///   <code>PS C:\>Get-SteamPackage -PackageId 73194 -Region "AT" -Language "spanish"</code>
+    ///   <para></para>
+    ///   <para>This command gets package with id 73194 with regional parameters like date format,</para>
+    ///   <para>price or currency for the austrian steam store, with strings localized to spanish.</para>
+    ///   <para></para>
+    /// </example>
     [Cmdlet(VerbsCommon.Get, "SteamPackage")]
     [OutputType(typeof(PackageInfo))]
     public class GetSteamPackageCmdlet : Cmdlet
     {
         #region properties
+        /// <summary>
+        /// <para type="description">Steam App ID</para>
+        /// </summary>
         [Parameter(Position = 0, Mandatory = true, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
         [Alias("id", "appid")]
         public int PackageId { get; set; }
 
+        /// <summary>
+        /// <para type="description">Two letter country code to customise currency and date values.</para>
+        /// </summary>
         [Parameter(Position = 1, ValueFromPipelineByPropertyName = true)]
-        [Alias("country")]
+        [Alias("country", "cc")]
         public string Region { get; set; }
 
+        /// <summary>
+        /// <para type="description">Full name of the language in english used for string localization e.g. name, description.</para>
+        /// </summary>
         [Parameter(Position = 2, ValueFromPipelineByPropertyName = true)]
         public string Language { get; set; }
 
