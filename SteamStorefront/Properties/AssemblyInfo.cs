9e9943ca26f29ec4290dba351825002570de8d98
mmuffins <mail@tiv73.com>
2018-05-29 23:20:24 +0200

    Updated assembly information.

--- SteamStorefront/Properties/AssemblyInfo.cs
+++ SteamStorefront/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SteamStorefront")]
-[assembly: AssemblyDescription("A powershell module that retrieves data from the Steam StoreFront API.")]
+[assembly: AssemblyDescription("Cmdlets that retrieve data from the Steam StoreFront API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Michael Kellner")]
 [assembly: AssemblyProduct("SteamStorefront")]
