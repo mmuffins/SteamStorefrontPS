cf1703001b7c050211c31d536becb11c64eeeeab
mmuffins <mail@tiv73.com>
2018-06-23 09:22:02 +0200

    Introduced proper error handling.

--- SteamStorefront/GetSteamFeaturedCategoryCmdlet.cs
+++ SteamStorefront/GetSteamFeaturedCategoryCmdlet.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -41,7 +42,20 @@ namespace SteamStorefront
         [Parameter(Position = 1, ValueFromPipelineByPropertyName = true)]
         public string Language { get; set; }
 
+        private SecurityProtocolType originalSecurityProtocol;
         #endregion
+
+        /// <summary>
+        /// <para type="description">BeginProcessing method</para>
+        /// </summary>
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            // It's not possible to set the security protocol per request, store the current configuration,
+            // update it to the values needed in the current call and restore it to its original setting 
+            originalSecurityProtocol = ServicePointManager.SecurityProtocol;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+        }
 
         /// <summary>
         /// <para type="description">ProcessRecord method</para>
@@ -49,11 +63,51 @@ namespace SteamStorefront
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
-            var results = Task.Run(async () => await FeaturedCategories.GetAsync(Region, Language)).Result.ToList();
-            if(results != null && results.Count > 0)
+
+            try
             {
-                results.ForEach(res => WriteObject(res));
+                var results = Task.Run(async () => await FeaturedCategories.GetAsync(Region, Language)).Result.ToList();
+                if (results != null && results.Count > 0)
+                {
+                    results.ForEach(res => WriteObject(res));
+                }
             }
+            catch (PipelineStoppedException)
+            {
+                // Nothing to do here, the try block is simply to handle exceptions when the user aborts the command
+                return;
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var error in ex.InnerExceptions)
+                {
+                    WriteError(new ErrorRecord(error, "UnknownError", ErrorCategory.NotSpecified, null));
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, "UnknownError", ErrorCategory.NotSpecified, null));
+                return;
+            }
+        }
+
+        /// <summary>
+        /// <para type="description">EndProcessing method</para>
+        /// </summary>
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+            ServicePointManager.SecurityProtocol = originalSecurityProtocol;
+        }
+
+        /// <summary>
+        /// <para type="description">StopProcessing method</para>
+        /// </summary>
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+            ServicePointManager.SecurityProtocol = originalSecurityProtocol;
         }
     }
 }
--- SteamStorefront/GetSteamPackageCmdlet.cs
+++ SteamStorefront/GetSteamPackageCmdlet.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -55,7 +56,20 @@ namespace SteamStorefront
         [Parameter(Position = 2, ValueFromPipelineByPropertyName = true)]
         public string Language { get; set; }
 
+        private SecurityProtocolType originalSecurityProtocol;
         #endregion
+
+        /// <summary>
+        /// <para type="description">BeginProcessing method</para>
+        /// </summary>
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+            // It's not possible to set the security protocol per request, store the current configuration,
+            // update it to the values needed in the current call and restore it to its original setting 
+            originalSecurityProtocol = ServicePointManager.SecurityProtocol;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+        }
 
         /// <summary>
         /// <para type="description">ProcessRecord method</para>
@@ -63,8 +77,47 @@ namespace SteamStorefront
         protected override void ProcessRecord()
         {
             base.ProcessRecord();
-            WriteObject(Task.Run(async () => await PackageDetails.GetAsync(PackageId, Region, Language)).Result);
+
+            try
+            {
+                WriteObject(Task.Run(async () => await PackageDetails.GetAsync(PackageId, Region, Language)).Result);
+            }
+            catch (PipelineStoppedException)
+            {
+                // Nothing to do here, the try block is simply to handle exceptions when the user aborts the command
+                return;
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var error in ex.InnerExceptions)
+                {
+                    WriteError(new ErrorRecord(error, "UnknownError", ErrorCategory.NotSpecified, null));
+                }
+                return;
+            }
+            catch (Exception ex)
+            {
+                WriteError(new ErrorRecord(ex, "UnknownError", ErrorCategory.NotSpecified, null));
+                return;
+            }
         }
 
+        /// <summary>
+        /// <para type="description">EndProcessing method</para>
+        /// </summary>
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+            ServicePointManager.SecurityProtocol = originalSecurityProtocol;
+        }
+
+        /// <summary>
+        /// <para type="description">StopProcessing method</para>
+        /// </summary>
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+            ServicePointManager.SecurityProtocol = originalSecurityProtocol;
+        }
     }
 }
