fae77937eca1a84056d7b8cbbf34aca292f01f7d
mmuffins <mail@tiv73.com>
2022-12-10 21:48:06 +0100

    Update to .net 7

---
+++ UpdateModuleManifest.ps1
@@ -0,0 +1,38 @@
+Param
+(
+    [string]$modulePath,
+    [string]$moduleId,
+    [Version]$moduleVersion,
+    [string]$moduleOwner,
+    [string]$moduleDescription,
+    [string]$moduleCopyright,
+    [string]$targetPath
+)
+
+$moduleRequireLicense = $false
+
+if(-not (Test-Path -Path $modulePath))
+{
+    Write-Error "Could not find $modulePath!"
+    return
+}
+
+## Update the file here
+Write-Host "Updating the module manifest file."
+
+Update-ModuleManifest -Path $modulePath -Author $moduleOwner -Copyright $moduleCopyright -Description $moduleDescription -ModuleVersion $moduleVersion -Guid $moduleId -RootModule $targetPath
+
+Write-Host "Validating the powershell module manifest."
+
+try
+{
+    Test-ModuleManifest -Path $modulePath -ErrorAction Stop
+}
+catch 
+{
+    $ErrorMessage = $_.Exception.Message
+    Write-Error "$ErrorMessage"
+    return
+}
+
+Write-Host "The module manifest is valid."
